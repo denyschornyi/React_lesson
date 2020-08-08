5736faa7cc217cf4b72a250291bc46b380024a90
Denys <chornijj14@gmail.com>
2020-08-08 10:42:55 +0200

    Create new component Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Denys gonna be a profesional in React</h1>
+      <Header />
     </div>
   );
 }
 
+
+const Header = () => {
+  return (
+    <ul>
+      <li>Main</li>
+      <li>Content</li>
+      <li>Contact</li>
+    </ul>
+  )
+}
 export default App;
