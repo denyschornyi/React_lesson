ba86de598606f78bca6ef82320416caaa5d882ea
Denys <chornijj14@gmail.com>
2020-08-08 20:51:29 +0200

    Fill nav section

--- src/App.js
+++ src/App.js
@@ -10,8 +10,12 @@ const App = () => {
       </header>
 
       <nav className="nav">
-        <div>Profile</div>
-        <div>Messages</div>
+        <div><a href="#">Profile</a></div>
+        <div><a href="#">Messages</a></div>
+        <div><a href="#">News</a></div>
+        <div><a href="#">Music</a></div>
+        <br/>
+        <div><a href="#">Settings</a></div>
       </nav>
 
       <div className="content">
