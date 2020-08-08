bb751d0d2cca98adf5924062674a5f7802dec476
Denys <chornijj14@gmail.com>
2020-08-08 21:11:20 +0200

    Brackdown into components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+          <img src="https://cdn.imgbin.com/9/19/11/imgbin-casual-diesel-clothing-fashion-logo-park-trail-WeDbU0uyNC3N50zBMDRukr0wu.jpg" alt="logo"/>
+      </header>
+    );
+}
+
+export default Header;
