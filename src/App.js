de748f997dacc27fca792d84f32f3c863206393b
Denys <chornijj14@gmail.com>
2020-08-08 21:21:35 +0200

    Make alone Content component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Content from './components/Content'
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
-
       <Nav />
-
-      <div className="content">
-        <div>
-          <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" alt=""/>  
-        </div>
-
-        <div>
-          ava + description
-        </div>
-
-        <div>
-          My posts
-          <div>New posts</div>
-        </div>
-
-        <div>
-          <div>Post 1</div>
-          <div>Post 2</div>
-        </div>
-
-      </div>
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+
+      <div className="content">
+        <div>
+            <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" alt=""/>  
+        </div>
+
+        <div>
+            ava + description
+        </div>
+
+        <div>
+            My posts
+            <div>New posts</div>
+        </div>
+
+        <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+        </div>
+
+    </div>
+    );
+}
+
+export default Content;
