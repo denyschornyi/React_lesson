0a2693336f3b4c74ba924f5c756864bbd69168d7
Denys <chornijj14@gmail.com>
2020-08-08 21:01:09 +0200

    Basic structure of the page

--- src/App.js
+++ src/App.js
@@ -19,8 +19,25 @@ const App = () => {
       </nav>
 
       <div className="content">
-        Main Content
+        <div>
+          <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" alt=""/>  
         </div>
+
+        <div>
+          ava + description
+        </div>
+
+        <div>
+          My posts
+          <div>New posts</div>
+        </div>
+
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+
+      </div>
     </div>
   );
 }
