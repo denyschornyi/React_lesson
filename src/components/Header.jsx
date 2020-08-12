043b6d57aaee1a86aa040025e3ff3bafba7ce30b
Denys <chornijj14@gmail.com>
2020-08-13 00:44:21 +0200

    Module structure for css code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Header.css'
+import classes from  './Header.module.css'
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
           <img src="https://cdn.imgbin.com/9/19/11/imgbin-casual-diesel-clothing-fashion-logo-park-trail-WeDbU0uyNC3N50zBMDRukr0wu.jpg" alt="logo"/>
       </header>
     );
