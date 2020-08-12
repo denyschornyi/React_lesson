c4f03082917b3c55c67f2756b748e73af1ef624a
Denys <chornijj14@gmail.com>
2020-08-13 00:30:38 +0200

    Use new conception of adding classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import './Nav.css'
+import classes from './Nav.module.css'
 
 const Nav = () => {
     return (
-        <nav className="nav">
-        <div><a href="#/">Profile</a></div>
-        <div><a href="#/">Messages</a></div>
-        <div><a href="#/">News</a></div>
-        <div><a href="#/">Music</a></div>
-        <br/>
-        <div><a href="#/">Settings</a></div>
-      </nav>
+        <nav className={classes.nav}>
+          <div className={classes.item}><a href="#/">Profile</a></div>
+          <div className={classes.item}><a href="#/">Messages</a></div>
+          <div className={classes.item}><a href="#/">News</a></div>
+          <div className={classes.item}><a href="#/">Music</a></div>
+          <br/>
+          <div className={classes.item}><a href="#/">Settings</a></div>
+        </nav>
     );
 }
 
