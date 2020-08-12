b2cd297be0c8c0e68e4d636b6105c23c45902cd0
Denys <chornijj14@gmail.com>
2020-08-13 00:34:54 +0200

    Create new changes with modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Profile.css';
+import classes from './Profile.module.css';
 const Profile = () => {
     return (
 
-      <div className="content">
+      <div className={classes.content}>
         <div>
             <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" alt=""/>  
         </div>
