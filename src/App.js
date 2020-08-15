d0cfcb545f4f9fd3ef7c1efc164c5a64627f4bc1
Denys <chornijj14@gmail.com>
2020-08-15 21:05:10 +0200

    Added new component Dialog

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile'
+import Dialogs from './components/Dialogs/Dialogs';
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Nav />
-      <Profile />
+      {/* <Profile /> */}
+      <Dialogs/>
     </div>
   );
 }
