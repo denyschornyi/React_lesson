9c950ee302d6d6e6eb3c023dac254c9ed85f6ccf
Denys <chornijj14@gmail.com>
2020-08-16 14:38:18 +0200

    Make a Dialog page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,22 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>Dialogs</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Denys</div>
+                <div className={classes.dialog}>Daria</div>
+                <div className={classes.dialog}>Yulia</div>
+                <div className={classes.dialog}>Maria</div>
+                <div className={classes.dialog}>Artemius</div>
+                <div className={classes.dialog}>Vova</div>
+                <div className={classes.dialog}>Alex</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Are you already a PRO in React?</div>
+            </div>
+        </div>
     );
 }
 
