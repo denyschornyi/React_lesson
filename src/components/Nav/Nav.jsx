40a0d3ef098e4f8b360883cdb6fc6cbe3a90fa68
Denys <chornijj14@gmail.com>
2020-08-16 14:02:58 +0200

    Learned that NavLink add to active tab class active, what we can style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,20 +6,20 @@ const Nav = () => {
     return (
         <nav className={classes.nav}>
           <div className={classes.item}>
-            <NavLink to="/profile">Profile</NavLink>
+            <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
           </div>
           <div className={classes.item}>
-            <NavLink to="/dialogs">Messages</NavLink>
+            <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
           </div>
           <div className={classes.item}>
-            <NavLink to="/news">News</NavLink>
+            <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
           </div>
           <div className={classes.item}>
-            <NavLink to="/music">Music</NavLink>
+            <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
           </div>
           <br/>
           <div className={classes.item}>
-            <NavLink to="/settings">Settings</NavLink>
+            <NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink>
           </div>
         </nav>
     );
