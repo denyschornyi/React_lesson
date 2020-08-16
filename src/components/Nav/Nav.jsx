10f47e7bb0a4e1fb04586105053d817a7f251746
Denys <chornijj14@gmail.com>
2020-08-16 13:41:03 +0200

    Learned what is a NavLink and replace a href to NavLink to

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import classes from './Nav.module.css'
 
 const Nav = () => {
     return (
         <nav className={classes.nav}>
           <div className={classes.item}>
-            <a href="/profile">Profile</a>
+            <NavLink to="/profile">Profile</NavLink>
           </div>
           <div className={classes.item}>
-            <a href="/dialogs">Messages</a>
+            <NavLink to="/dialogs">Messages</NavLink>
           </div>
           <div className={classes.item}>
-            <a href="/news">News</a>
+            <NavLink to="/news">News</NavLink>
           </div>
           <div className={classes.item}>
-            <a href="/music">Music</a>
+            <NavLink to="/music">Music</NavLink>
           </div>
           <br/>
           <div className={classes.item}>
-            <a href="/settings">Settings</a>
+            <NavLink to="/settings">Settings</NavLink>
           </div>
         </nav>
     );
