e825859f68e470ae50ea2f92141543bb92752481
Denys <chornijj14@gmail.com>
2020-08-19 00:04:23 +0200

    Individual copmonent for ProfileInfo

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './ProfileInfo.module.css';
+
+const ProfileInfo = (props) => {
+    return(
+        <React.Fragment>
+            <div>
+                <img src="https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg" alt=""/>  
+            </div>
+
+            <div>
+                 ava + description
+            </div>
+        </React.Fragment>
+    );
+}
+export default ProfileInfo;
