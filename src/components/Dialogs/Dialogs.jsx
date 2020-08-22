7766ebf9e78c31eb167671354afb653e2d781a8e
Denys <chornijj14@gmail.com>
2020-08-22 19:04:10 +0200

    Replace Arrays of dialogData and messageData to App.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,25 +6,9 @@ import MessageItem from './MessageItem/MessageItem';
 import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
-
-    let dialogData = [
-        {id: 1, name: 'Denys'},
-        {id: 2, name: 'Daria'},
-        {id: 3, name: 'Maria'},
-        {id: 4, name: 'Artemus'},
-        {id: 5, name: 'Vova'},
-        {id: 6, name: 'Alex'},
-    ];
-
-
-    let messageData = [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Are you already a PRO in React?'}
-    ];
  
-    let dialogElements = dialogData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
-    let messagesElements = messageData.map(message => <MessageItem message={message.message}/>)
+    let dialogElements = props.dialogData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+    let messagesElements = props.messageData.map(message => <MessageItem message={message.message}/>)
 
     return (
         <div className={classes.dialogs}>
